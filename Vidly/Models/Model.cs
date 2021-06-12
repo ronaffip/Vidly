534ad4d8758e2c3edee3a10fc1495dd439675899
Rohit <ronaffip@gmail.com>
2021-06-13 01:19:43 +0530

    Video 25 Code First Migrations

---
+++ Vidly/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    public class Model : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
