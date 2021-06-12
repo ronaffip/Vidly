a7c2342192b243e1a8c536a08e653e81a874eee3
Rohit <ronaffip@gmail.com>
2021-06-12 16:26:07 +0530

    Mosh Exercise 2 solutions

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -38,17 +38,20 @@ namespace Vidly.Controllers
         }
 
         //Movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
         {
-            if (!pageIndex.HasValue)
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
             {
-                pageIndex = 1;
-            }
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-            return Content(String.Format("pageIndex = {0} & sortBy = {1}",pageIndex,sortBy));
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
